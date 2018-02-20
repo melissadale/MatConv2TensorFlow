f5d5f724fd1932ad2524ef79d20f56ab43ccb1b6
Melissa <melissa.r.dale@gmail.com>
2018-02-19 22:03:21 -0500

    Silly Me, I didn't need to "rotate" things

--- MatLab2Python.py
+++ MatLab2Python.py
@@ -4,6 +4,8 @@ import numpy as np
 """
 Below are taken from:
 https://stackoverflow.com/questions/7008608/scipy-io-loadmat-nested-structures-i-e-dictionaries
+
+Needed to properly scrape the dictionaries from matconvnet model 
 """
 def loadmat(filename):
     '''
@@ -40,30 +42,16 @@ def _todict(matobj):
 
 
 """
-Scrape data from matconvnet mat file 
+Now, we need to Scrape relevant data 
+from model dictionary 
 """
-def rotate_weights(weights):
-    new_matrx = []
-
-    for i in range(np.size(weights, 2)):
-        row0 = [weights[0][0][i], weights[1][0][i], weights[2][0][i], weights[3][0][i]]
-        row1 = [weights[0][1][i], weights[1][1][i], weights[2][1][i], weights[3][1][i]]
-        row2 = [weights[0][2][i], weights[1][2][i], weights[2][2][i], weights[3][2][i]]
-        row3 = [weights[0][3][i], weights[1][3][i], weights[2][3][i], weights[3][3][i]]
-
-        new_matrx.append(np.array([row0, row1, row2, row3]).transpose())
-
-    return new_matrx
-
-
 def model_creation(path_to_model):
     data = loadmat(path_to_model)
     model = []
 
     for i in data['net']['layers']:
         if i.type == 'conv':
-            weights = rotate_weights(i.weights[0])
-            model.append({'weights': weights, 'bias': i.weights[1], 'stride': i.stride, 'padding': i.pad, 'momentum': i.momentum,'lr': i.learningRate,'weight_decay': i.weightDecay})
+            model.append({'weights': i.weights[0], 'bias': i.weights[1], 'stride': i.stride, 'padding': i.pad, 'momentum': i.momentum,'lr': i.learningRate,'weight_decay': i.weightDecay})
 
         # RELU
         else:
